3149ef998d29374c53b4d030043b4d569f7ff7df
Minh Hau <haunguyenvn2k3@gmail.com>
2024-04-17 11:03:26 +0700

    feat: update controller

--- src/main/java/com/sportplatform/RESTAPI/HomeRestController.java
+++ src/main/java/com/sportplatform/RESTAPI/HomeRestController.java
@@ -3,9 +3,9 @@ package com.sportplatform.RESTAPI;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/home")
 public class HomeRestController {
-    @GetMapping("/home")
+    @GetMapping
     public String getHome(){
         return "Hello World";
     }
