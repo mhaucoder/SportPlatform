e5291b582a7e905c8995028835e24e6a6a0310ca
Minh Hau <haunguyenvn2k3@gmail.com>
2024-04-17 09:56:59 +0700

    feat: update Product API

--- src/main/java/com/sportplatform/RESTAPI/ProductRestController.java
+++ src/main/java/com/sportplatform/RESTAPI/ProductRestController.java
@@ -1,4 +1,12 @@
 package com.sportplatform.RESTAPI;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("product")
 public class ProductRestController {
+    @GetMapping
+    public String getProduct() {
+        return "Get Product";
+    }
 }
